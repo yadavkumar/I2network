6a4a95d15acad699c0188123be41a21c8cf2e0ef
yadavkumar <connectyadav@gmail.com>
2018-07-18 21:49:39 +0530

    added socket class to general utility

--- I2NetWorks/I2NetWorks/GeneralUtility.h
+++ I2NetWorks/I2NetWorks/GeneralUtility.h
@@ -4,5 +4,16 @@ class GeneralUtility
 public:
 	GeneralUtility(void);
 	~GeneralUtility(void);
+	class socketcommunication{
+	public :
+		socketcommunication();
+		~socketcommunication();
+		int opensocketConnection();
+		int closesocketConnection();
+		int GetDataFromConnection();
+		int SendDataToConnection();
+		int ListenOnPort();
+
+	};
 };
 
